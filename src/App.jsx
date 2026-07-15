228542ac295b887a3d81a360cf26f6b7a7bb2363
avanycmurali <avanycmuralidharan@gmail.com>
2026-07-15 11:56:26 +0530

    AddBookings successfull

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import AddMovies from './components/AddMovies'
 import Home from './components/HOme'
+import AddBooking from './components/AddBookings'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
      <AddMovies/>
+     <AddBooking/>
     </>
   )
 }
